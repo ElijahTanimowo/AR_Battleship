3d60c9a8d40b0a91d2ce468d54ecc49dd6f2cae3
RalphRobes0324 <ralphrobes123@gmail.com>
2024-11-02 16:45:04 -0400

    Player is able select a grid point

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -20,7 +20,51 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SpawnGrid();
+        SelectGridPoint();
+    }
 
+    private void SelectGridPoint()
+    {
+        //Grid exist
+        if (spawnedObject != null)
+        {
+            //Check for mouse input
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    GameObject selectedGridPoint = hit.collider.gameObject;
+                    if (selectedGridPoint.tag == "EnemyGridPoint")
+                    {
+                        Debug.Log("Selected Grid Point: " + selectedGridPoint.name);
+                    }
+                }
+            }
+            //Check for touch screen
+            else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Touch touch = Input.GetTouch(0);
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    GameObject selectedGridPoint = hit.collider.gameObject;
+                    if (selectedGridPoint.tag == "EnemyGridPoint")
+                    {
+                        Debug.Log("Selected Grid Point: " + selectedGridPoint.name);
+                    }
+                }
+            }
+        }
+    }
+
+    private void SpawnGrid()
+    {
         if (spawnedObject != null)
             return;  // Exit if we already have a box spawned
 
