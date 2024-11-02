01c6166018804d5196cf3682b41b485ee4c94d87
RalphRobes0324 <ralphrobes123@gmail.com>
2024-11-02 16:52:21 -0400

    Checks if player hits the grid

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -32,28 +32,34 @@ public class Player : MonoBehaviour
             //Check for mouse input
             if (Input.GetMouseButtonDown(0))
             {
+                //Check where screen hit
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
 
                 if (Physics.Raycast(ray, out hit))
                 {
                     GameObject selectedGridPoint = hit.collider.gameObject;
+                    //Check it is Enemy Grid Point
                     if (selectedGridPoint.tag == "EnemyGridPoint")
                     {
                         Debug.Log("Selected Grid Point: " + selectedGridPoint.name);
+                        CheckForShip(selectedGridPoint);
                     }
                 }
             }
             //Check for touch screen
             else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
+                //Check for screen touch input
                 Touch touch = Input.GetTouch(0);
+                //Check where screen hit
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
 
                 if (Physics.Raycast(ray, out hit))
                 {
                     GameObject selectedGridPoint = hit.collider.gameObject;
+                    //Check it is Enemy Grid Point
                     if (selectedGridPoint.tag == "EnemyGridPoint")
                     {
                         Debug.Log("Selected Grid Point: " + selectedGridPoint.name);
@@ -83,6 +89,15 @@ public class Player : MonoBehaviour
                 // Spawn the object at the hit point if nothing is spawned yet
                 spawnedObject = Instantiate(objectToSpawn, hit.pose.position, objectToSpawn.transform.rotation);
             }
+        }
+    }
+
+    void CheckForShip(GameObject gridPoint)
+    {
+        GridPoint point = gridPoint.GetComponent<GridPoint>();
+        if (point != null) 
+        {
+            point.OnHitGridPoint();
         }
     }
 }
