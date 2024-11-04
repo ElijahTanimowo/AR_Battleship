b961577b06768085d82ae459519dc96bfc35b66e
RalphRobes0324 <ralphrobes123@gmail.com>
2024-11-03 20:11:42 -0500

    Display Winner Text

--- Assets/Scripts/Managers/DisplayWinner.cs
+++ Assets/Scripts/Managers/DisplayWinner.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class DisplayWinner : MonoBehaviour
 {
-
     [SerializeField] TextMeshProUGUI displayWinnerText;
     [SerializeField] GameObject displayObject;
 
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,9 +1,20 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    [SerializeField] TextMeshProUGUI displayWinnerText;
+    [SerializeField] GameObject displayObject;
+    [SerializeField] Button playerButton;
+    [SerializeField] GameObject playObj;
+
+
+    [SerializeField] GameObject displayCanvas;
+
     static UIManager _instance = null;
     public TMP_Text title;
 
@@ -22,6 +33,50 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        FindWinTextObj();
+        DisplayWinner();
+    }
+
+    private void FindWinTextObj()
+    {
+        if (displayObject == null)
+        {
+
+            displayObject = GameObject.Find("WhoWonText");
+            if (displayObject != null)
+            {
+                playObj = GameObject.Find("Button");
+                displayWinnerText = displayObject.GetComponent<TextMeshProUGUI>();
+                playerButton = playObj.GetComponent<Button>();
+                //playerButton.onClick.AddListener(onPlay);
+            }
+        }
+    }
+
+    private void DisplayWinner()
+    {
+
+        if (UIManager.Instance != null && displayWinnerText != null)
+        {
+            if (UIManager.Instance.player1Won)
+            {
+                displayWinnerText.text = "Player 1 Won";
+                displayObject.SetActive(true);
+            }
+            else if (UIManager.Instance.player2Won)
+            {
+                displayWinnerText.text = "Player 2 Won";
+                displayObject.SetActive(true);
+            }
+            else
+            {
+                displayWinnerText.text = "";
+            }
+        }
+    }
+
     void Awake()
     {
         title = GameObject.FindGameObjectWithTag("TitleText").GetComponent<TMP_Text>();
@@ -34,6 +89,8 @@ public class UIManager : MonoBehaviour
             _instance = this;
             DontDestroyOnLoad(gameObject);
         }
+        FindWinTextObj();
+
     }
     // Can be used to have strings used to change Scenes
     public void OnSceneChange(string name)
